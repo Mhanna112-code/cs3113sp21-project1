8ab00cb564b0e98285b233a9026c39586a0b6b66
Mhanna112-code <60158039+Mhanna112-code@users.noreply.github.com>
2021-04-08 16:35:11 -0500

    correct logic

--- project1.c
+++ project1.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <cstdlib>
 
 struct Process{
     int ID;
     int runTime;
     int priority;
-    int flag1;
-    int flag2;
-    int flag3;
-    int lastIdx;
-}processArr[2000];
+    int lastIdxFlag = 0;
+    int flag2 = 0;
+    int flag3 = 0;
+    int lastIdx = 0;
+}processArr[100];
 
-int findIndex(struct Process arr[], int idx, int K)
+int findIndex(Process arr[], int idx, int K)
 {
     if (idx < 0)
         return -1;
@@ -22,21 +23,37 @@ int findIndex(struct Process arr[], int idx, int K)
     // Recursive Call
     return findIndex(arr, idx - 1, K);
 }
+
+int countNumDistinctElements(struct Process arr[], int n)
+{
+    int numVol = 1;
+
+    // Pick all elements one by one
+    for (int i = 1; i < n; i++) {
+        int j = 0;
+        for (j = 0; j < i; j++)
+            if (arr[i].ID == arr[j].ID)
+                break;
+
+        // If not printed earlier, then print it
+        if (i == j)
+            numVol++;
+    }
+    return numVol;
+}
+
 int nProcesses, nExecutionElements, nInstructions;
 
 int main(int argc, char **argv) {
-    FILE *f = fopen(argv[1], "r");
+    FILE *f = fopen("C:\\Users\\march\\CLionProjects\\cs3113-project1\\sample", "r"); // "r" for read
     int arrLength = sizeof(processArr) / sizeof(processArr[0]);
-    for (int k = 0; k < arrLength; k++){
-        processArr[k].flag1 = 0;
-        processArr[k].flag2 = 0;
-        processArr[k].flag3 = 0;
-        processArr[k].lastIdx = 0;
-
+    for (int i = 0; i < arrLength; i++) {
+        processArr[i].lastIdxFlag = 0;
+        processArr[i].flag2 = 0;
+        processArr[i].flag3 = 0;
+        processArr[i].lastIdx = 0;
     }
-    int flag = 0;
-    int nonVol = 0;
-    int vol = 0;
+    int contextSwitches = 0;
     fscanf(f, "%d", &nProcesses);
     fscanf(f, "%d", &nExecutionElements);
     fscanf(f, "%d", &nInstructions);
@@ -47,51 +64,48 @@ int main(int argc, char **argv) {
         fscanf(f, "%d", &processArr[i].ID);
         fscanf(f, "%d", &processArr[i].runTime);
         fscanf(f, "%d", &processArr[i].priority);
-        if (i != 0) {
-            for (int j = 0; j < nInstructions; j++) {
-                if (processArr[j].ID == processArr[i].ID && j != i && j != i - 1 && j != i + 1 && processArr->flag1 != 1) {
-                    flag = 1;
-                    nonVol++;
-                    processArr[i].ID = 1;
-                }
-                if (flag == 0)
-                    vol++;
-                flag = 1;
+        if (i == 0) {
+            if (processArr[i].ID != processArr[i + 1].ID)
+                contextSwitches++;
+        }
+        else if (i > 0 && i < nInstructions - 1)
+            if ((processArr[i].ID != processArr[i + 1].ID) && (processArr[i].ID != processArr[i + 1].ID))
+                contextSwitches++;
+        else
+            if (processArr[i].ID != processArr[i - 1].ID)
+                contextSwitches++;
+    }
+    float numVol = countNumDistinctElements(processArr,nInstructions);
+    int numNonVol = contextSwitches - numVol;
+    float cpuUtil = 100.00;
+    float avgTurnAroundTime;
+    int totalBurstTime = 0;
+    float totalRunTime = 0;
+    int lastIdx;
+    int currTurnAroundTime = 0;
+    for (int k = 0; k < nInstructions; k++) {
+        totalRunTime += processArr[k].runTime;
+        lastIdx = findIndex(processArr, arrLength - 1, processArr[k].ID);
+        if (processArr[k].lastIdxFlag != 1){
+            for (int j = 0; j <= lastIdx; j++) {
+                currTurnAroundTime += processArr[j].runTime;
+                avgTurnAroundTime += processArr[j].runTime;
             }
-            flag = 0;
+            for (int u = 0; u <= lastIdx; u++) {
+                if (processArr[u].ID == processArr[lastIdx].ID)
+                    totalBurstTime += processArr[u].runTime;
+            }
+                waitingTime += currTurnAroundTime - totalBurstTime;
+                totalBurstTime = 0;
+                currTurnAroundTime = 0;
+                processArr[lastIdx].lastIdxFlag = 1;
         }
     }
-    float cpuUtil = 100.00;
-    float throughPutNum = nExecutionElements;
-    float throughPutDenom = 0;
-    float avgResTimeNum = 0;
-    float avgResTimeDenum = nExecutionElements;
-    float avgTurnAroundTimeNum = 0;
-    float avgTurnAroundTimeDenum  = nExecutionElements;
-    float avgTurnAroundTime;
-    for (int k = 0; k < nInstructions; k++) {
-        throughPutDenom += processArr[k].runTime;
-        if (processArr[k].priority != 1)
-            waitingTime += processArr[k].priority;
-        if (k == 0)
-            avgResTimeNum += 1;
-        else
-            avgResTimeNum += processArr[k - 1].runTime + 1;
-        processArr[k].lastIdx = findIndex(processArr, arrLength-1, processArr[k].ID);
-    }
-    for (int b = 0; b < nInstructions; b++) {
-            if (processArr[processArr[b].lastIdx].flag3 != 1) {
-                for (int c = 0; c <= processArr[b].lastIdx; c++)
-                    avgTurnAroundTimeNum += processArr[c].runTime;
-                processArr[processArr[b].lastIdx].flag3 = 1;
-            }
-    }
-    throughPut = throughPutNum/throughPutDenom;
-    waitingTime /= nExecutionElements;
-    avgResTime = avgResTimeNum/avgResTimeDenum;
-    avgTurnAroundTime = avgTurnAroundTimeNum/avgTurnAroundTimeDenum;
-    printf("%d\n", vol);
-    printf("%d\n", nonVol);
+    avgTurnAroundTime /= numVol;
+    waitingTime /= numVol;
+    throughPut = numVol / totalRunTime;
+    printf("%d\n", numVol);
+    printf("%d\n", numNonVol);
     printf("%.2f\n", cpuUtil);
     printf("%.2f\n", throughPut);
     printf("%.2f\n", avgTurnAroundTime);
@@ -99,4 +113,3 @@ int main(int argc, char **argv) {
     printf("%.2f\n", avgResTime);
     return 0;
 }
-                               
